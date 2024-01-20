11f047b1304be8bf2a682ce2290dc9adf4f93666
Cameron <camharris831@gmail.com>
2024-01-19 19:27:51 -0800

    player page image header

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, Text} from 'react-native';
 import { common } from '../stylesheets/styles';
-import PlayerSearch from '../components/PlayerSearch';
-
 
 const SettingsScreen = () => {
+
+
   return (
-    <View style={common.screenContainer}>
-      <PlayerSearch/>
+    <View>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/test.png')}
+          style={styles.image}
+        />
+      </View>
+
     </View>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    height: '50%', // Set the height to the value you want
+    overflow: 'hidden',    // This hides the part of the image outside the container
+    backgroundColor: '#0268d6'
+  },
+  image: {
+    width: '60%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+    top: 5,               // Align the top of the image with the container
+  },
+});
 
 export default SettingsScreen;
